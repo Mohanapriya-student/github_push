4f09f86ce2f9e45093bd4633ff22b00b97acfa1b
Mohanapriya-student <mohanapriyamanivel34@gmail.com>
2026-09-03 10:22:41 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,24 @@
+//no Exception 
+public class Ex1{
+public static void main(String[] ar){
+try{
+String s1=ar[0];
+
+String s2=ar[1];
+
+System.out.println(s1+s2);
+
+int a=Integer.parseInt(s1);
+
+int b=Integer.parseInt(s2);
+
+System.out.println(a+b);
+
+System.out.println(a/b);
+}catch(Exception e){
+
+System.out.println("Please enter two numbers to add and divide"+e);
+}
+
+}
+}
