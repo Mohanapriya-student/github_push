d06a099d72311a4066693d1d29716b3de2d8a7d3
Mohanapriya-student <mohanapriyamanivel34@gmail.com>
2026-09-03 10:22:03 +0530

    Add files via upload

---
+++ Testcommandinarg.java
@@ -0,0 +1,17 @@
+class Testcommandinarg{
+public static void main(String[] ar){
+String s1=ar[0];
+
+String s2=ar[1];
+
+System.out.println(s1+s2);
+
+int a=Integer.parseInt(s1);
+
+int b=Integer.parseInt(s2);
+
+System.out.println(a+b);
+
+System.out.println(a/b);
+}
+}
