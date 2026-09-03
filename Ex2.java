9362c3383a1c272f662ae36bb120b97d3593985d
Mohanapriya-student <mohanapriyamanivel34@gmail.com>
2026-09-03 10:23:17 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,28 @@
+//exception handling using try catch finally block
+//no Exception handling 
+public class Ex2{
+public static void main(String[] ar){
+try{
+String s1=ar[0];
+
+String s2=ar[1];
+
+System.out.println(s1+s2);
+
+int a=Integer.parseInt(s1);
+
+int b=Integer.parseInt(s2);
+
+System.out.println(a+b);
+
+System.out.println(a/b);
+}catch(Exception e){
+
+System.out.println("Please enter two numbers to add and divide"+e);
+}
+finally{
+System.out.println("finally block reached");
+}
+
+}
+}
